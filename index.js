60864076e3c8d6a0d1adcab551e3d00f897b6ed5
Lingyong Sun <lysz210@gmail.com>
2021-08-25 12:47:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+/**
+ * Pass the data to send as `event.data`, and the request options as
+ * `event.options`. For more information see the HTTPS module documentation
+ * at https://nodejs.org/api/https.html.
+ *
+ * Will succeed with the response body.
+ */
+exports.handler = async (event, context) => {
+    return "hello world"
+};
