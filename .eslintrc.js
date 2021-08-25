af6e0451e212f12c9bf49cb4736ac7a58e7c754f
Lingyong Sun <lysz210@gmail.com>
2021-08-25 19:44:09 +0200

    update

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  extends: [
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {}
+}
